e0a57258cfdabba3e23b35b0587f7736abd66148
arun <arunkumar629@gmail.com>
2021-05-03 12:32:51 +0530

    prophet_added:

--- forecast_model/prophet.py
+++ forecast_model/prophet.py
@@ -1,6 +1,7 @@
 import os
 from werkzeug.utils import secure_filename
 import pandas as pd
+from statsmodels.tools.eval_measures import rmse
 from fbprophet import Prophet
 
 
@@ -15,7 +16,22 @@ class ProphetModel:
         forecast = m.predict(future)
         filename=filename[:-4]
         m.plot(forecast).savefig(os.path.join('static/', secure_filename(filename+'_prophetPredict.jpg')))
-
+        # 80% for training
+        train = df.iloc[:int(len(df)*0.8)]
+        test = df.iloc[len(train):]
+        m = Prophet()
+        m.fit(train)
+        future = m.make_future_dataframe(periods=len(test),freq='MS')
+        forecast = m.predict(future)
+        #print(forecast.tail())
+        ax = forecast.plot(x='ds',y='yhat',label='Predictions',legend=True,figsize=(12,8))
+        g=test.plot(x='ds',y='y',label='True Miles',legend=True,ax=ax,xlim=('2018-01-01','2019-01-01'))
+        g.figure.savefig(os.path.join('static/', secure_filename(filename+'_prophetCompare.jpg')))
+        predictions = forecast.iloc[len(train):]['yhat']
+        error=rmse(predictions,test['y'])
+        mean=test.mean()
+        print('percentage')
+        print(100-(error/mean*100))
 
 #a=ProphetModel()
 #a.execute('BeerWineLiquor.csv')
